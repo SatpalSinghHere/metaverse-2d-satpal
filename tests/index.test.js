921d76c5bd09f5d74e1fc2a2ba8251a38df3cd11
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-11-07 10:50:06 +0530

    Added authentication tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -23,4 +23,40 @@ describe("Authenticate", ()=>{
         })
         expect(updatedResponse.status).toBe(400)
     })
+
+    test("Signup fails if the username is empty", async()=>{
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            password: "password", type:"admin"
+        })
+        expect(response.status).toBe(400)
+    })
+
+    test('Signin succeeds with correct credentials', async()=>{
+        const username = "satpal"+Math.random()
+        const password = "password"
+
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username, password, type:"admin"
+        })
+
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+            username, password, type:"admin"
+        })
+        expect(response.status).toBe(200)
+        expect(response.body.token).toBeDefined()
+    })
+
+    test('Signin fails with incorrect credentials', async()=>{
+        const username = "satpal"+Math.random()
+        const password = "password"
+
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username, password, type:"admin"
+        })
+
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+            username, password: "wrongpassword", type:"admin"
+        })
+        expect(response.status).toBe(403)
+    })
 })
