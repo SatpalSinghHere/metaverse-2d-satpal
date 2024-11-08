f94de55c1fa8c60ff1e36a82cf3f09f636fe72c9
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-11-08 21:53:06 +0530

    Fininshing up http routes skeleton

--- metaverse-2d/apps/http/src/routes/v1/index.ts
+++ metaverse-2d/apps/http/src/routes/v1/index.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express'
+import { userRouter } from './user'
+import { adminRouter } from './admin'
+import { spaceRouter } from './space'
 
 export const router = Router()
 
@@ -14,6 +17,10 @@ router.get('/signin', (req, res)=>{
     })
 })
 
-router.get('elements')
+router.get('/elements')
 
-router.get('avatars')
+router.get('/avatars')
+
+router.use('/user', userRouter)
+router.use('/admin', adminRouter)
+router.use('/space', spaceRouter)
